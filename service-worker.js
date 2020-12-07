c2f82c49b1ec817bc0989f284b8035cc1583bbe0
iamowaisazam <iamowaisazam@gmail.com>
2020-12-08 00:01:04 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/owaiscommerce/precache-manifest.807dae99d40fba787abb10d2dc185d82.js"
+  "/owaiscommerce/precache-manifest.0e969072f80b8422519486910a331859.js"
 );
 
 self.addEventListener('message', (event) => {
